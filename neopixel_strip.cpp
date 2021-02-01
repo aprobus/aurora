33e70a2614196f5c2ee4277af5f7bb6d15ea68d4
Aaron Probus <aprobus15@gmail.com>
2021-01-31 18:58:33 -0800

    Fixes leftover neopixel references

--- neopixel_strip.cpp
+++ neopixel_strip.cpp
@@ -7,34 +7,34 @@
 
 #include "led_strip.h"
 
-NeopixelWrapper::NeopixelWrapper(Adafruit_NeoPixel* pixels) : pixels_(pixels) {}
+NeopixelStrip::NeopixelStrip(Adafruit_NeoPixel* pixels) : pixels_(pixels) {}
 
-void NeopixelWrapper::begin() {
+void NeopixelStrip::begin() {
   pixels_->begin();
 }
 
-void NeopixelWrapper::show() {
+void NeopixelStrip::show() {
   if (is_dirty_) {
     pixels_->show();
     is_dirty_ = false;
   }
 }
 
-void NeopixelWrapper::setPixelColor(uint16_t index, color_t color) {
+void NeopixelStrip::setPixelColor(uint16_t index, color_t color) {
   pixels_->setPixelColor(index, color);
   is_dirty_ = true;
 }
 
-void NeopixelWrapper::clear() {
+void NeopixelStrip::clear() {
   pixels_->clear();
   is_dirty_ = true;
 }
 
-uint16_t NeopixelWrapper::numPixels() const {
+uint16_t NeopixelStrip::numPixels() const {
   return pixels_->numPixels();
 }
 
-color_t NeopixelWrapper::getPixelColor(uint16_t index) const {
+color_t NeopixelStrip::getPixelColor(uint16_t index) const {
   return pixels_->getPixelColor(index);
 }
 
